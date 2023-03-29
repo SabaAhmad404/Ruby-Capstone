d5095038e3f68f3511c94fe2426598f7ce6b1d70
haliljon <110017001+haliljon@users.noreply.github.com>
2023-03-29 13:16:23 +0500

    Fix linters

--- book/book.rb
+++ book/book.rb
@@ -2,23 +2,23 @@ require_relative '../item'
 require 'json'
 
 class Book < Item
-    attr_accessor :label, :publisher, :cover_state
+  attr_accessor :label, :publisher, :cover_state
 
-    def initialize(publisher, cover_state, publish_date)
-        super(publish_date)
-        @publisher = publisher
-        @cover_state = cover_state
-    end
+  def initialize(publisher, cover_state, publish_date)
+    super(publish_date)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
 
-    def can_be_archived?
-        super || cover_state == "bad"
-    end
+  def can_be_archived?
+    super || cover_state == 'bad'
+  end
 
-    def to_json(*_args)
-        {
-            "publish_date" => @publish_date,
-            "publisher" => @publisher,
-            "cover_state" => @cover_state
-        }.to_json(*_args)
-    end
+  def to_json(*args)
+    {
+      'publish_date' => @publish_date,
+      'publisher' => @publisher,
+      'cover_state' => @cover_state
+    }.to_json(*args)
+  end
 end
